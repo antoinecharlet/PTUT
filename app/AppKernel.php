28c8df8c8d45cb64d8fb0f691d956e0d09adef80
ArthurdeBarbanson <arthurdebarbanson@gmail.com>
2016-03-09 11:39:06 +0100

    rename SiteBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new HelloWordBundle\HelloWordBundle(),
-            new EntrepriseBundle\EntrepriseBundle(),
+            new SiteBundle\SiteBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
